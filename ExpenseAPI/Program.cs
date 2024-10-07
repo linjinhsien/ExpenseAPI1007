16b0a91d7e8d7449502c66355c8ad3ad6fcf0d82
linjinhsienmsi <iven840320@gmail.com>
2024-10-07 22:12:08 +0800

    加入專案檔案。

---
+++ ExpenseAPI/Program.cs
@@ -0,0 +1,40 @@
+using ExpenseAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//DI�`�J
+builder.Services.AddDbContext<ExpenseContext>(options =>
+    options.UseInMemoryDatabase("ExpenseList"));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
